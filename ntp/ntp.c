56d86d4eda504e399d96389fb7b8e0b65b4b7a83
armink <armink.ztl@gmail.com>
2019-08-20 11:00:22 +0800

    【完善】ntp 头文件引用。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- ntp/ntp.c
+++ ntp/ntp.c
@@ -28,7 +28,7 @@
 #include <sys/types.h>
 #include <rtdevice.h>
 
-#if defined(RT_USING_SAL) && defined(RT_USING_NETDEV)
+#if defined(RT_USING_SAL)
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netdb.h>
