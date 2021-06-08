90a35e8fea50ff9176bcf6ef36e02deae640bdd7
fsdf <hk517j@qq.com>
2021-06-08 13:42:20 +0800

    小补一下，设置keepalive失败返回

--- telnet/telnet.c
+++ telnet/telnet.c
@@ -342,6 +342,7 @@ static void telnet_thread(void* parameter)
     if(setsockopt(telnet->server_fd, SOL_SOCKET, SO_KEEPALIVE, (void *)&keepalive, sizeof(keepalive)) < 0)
 	{
 		rt_kprintf("telnet:set socket keepalive failed\n");
+        return;
 	}
 
     addr.sin_family = AF_INET;
